20cad01b00d09f5c863c1cc98d2d52874e470f36
Shaina-bit <shainanoushad0305@gmail.com>
2025-09-11 12:13:43 +0530

    added logical not example.

---
+++ logicalNot.java
@@ -0,0 +1,12 @@
+package Java_Module_2;
+
+public class logicalNot {
+    public static void main(String[] args) {
+        boolean isRaining= false;
+        if(!isRaining){
+            System.out.println("You can go outside without an umbrella.");
+        }else{
+            System.out.println("Take an umbrella!");
+        }
+    }
+}
