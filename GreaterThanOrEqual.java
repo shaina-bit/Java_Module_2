1855a75721541261d82dd2198b8e4cdc1666563a
Shaina-bit <shainanoushad0305@gmail.com>
2025-09-11 12:00:02 +0530

    added greater than or equal to file.

---
+++ GreaterThanOrEqual.java
@@ -0,0 +1,8 @@
+package Java_Module_2;
+
+public class GreaterThanOrEqual {
+    public static void main(String[] args){
+        int x=20,y =20;
+        System.out.println("x>=y: "+ (x>=y));
+    }
+}
