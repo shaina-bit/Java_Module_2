f6512d0a638b1bc1046b2d2d4e0ad93b602d1a9a
Shaina-bit <shainanoushad0305@gmail.com>
2025-09-15 12:01:20 +0530

    added ternary program.

---
+++ ternaryEx.java
@@ -0,0 +1,8 @@
+package Java_Module_2;
+public class ternaryEx {
+    public static void main(String[] args) {
+        int number=10;
+        String result= (number%2==0)?"Even":"Odd";
+        System.out.println("The number" + number+ "is"+ result);
+    }
+}
