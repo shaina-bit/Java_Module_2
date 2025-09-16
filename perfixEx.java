8d0fcbfb21e068a0ec3ba983a5bcfdfc9df49773
Shaina-bit <shainanoushad0305@gmail.com>
2025-09-16 15:08:53 +0530

    added prefix ex.

---
+++ perfixEx.java
@@ -0,0 +1,9 @@
+package Java_Module_2;
+
+public class perfixEx {
+    public static void main(String[] args) {
+        int a=5,b=3;
+        int result= ++a*b+2;
+        System.out.println("Result: " + result);
+    }
+}
