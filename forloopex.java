0eaf80038f9a02909d9959421feeeb5dbd15c5af
Shaina-bit <shainanoushad0305@gmail.com>
2025-09-22 12:24:53 +0530

    added a for loop ex.

---
+++ forloopex.java
@@ -0,0 +1,9 @@
+package Java_Module_2;
+
+public class forloopex {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            System.out.println("Count: " + i);
+        }
+    }
+}
