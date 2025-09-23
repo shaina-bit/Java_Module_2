b5bf1f8a2f08690e39ffa61b3baf86f74d843621
Shaina-bit <shainanoushad0305@gmail.com>
2025-09-23 15:01:45 +0530

    added for each loop example.

---
+++ forEach.java
@@ -0,0 +1,10 @@
+package Java_Module_2;
+
+public class forEach {
+    public static void main(String[] args) {
+        String[] fruits= {"apple", "mango","cherry"};
+        for(String fruit:fruits){
+            System.out.println("Fruit: "+ fruit);
+        }
+    }
+}
