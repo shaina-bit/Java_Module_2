f228cd14faee100ca83643cb43b3ebbca97afb89
Shaina-bit <shainanoushad0305@gmail.com>
2025-09-23 14:59:22 +0530

    added a do while program.

---
+++ doWhile.java
@@ -0,0 +1,10 @@
+package Java_Module_2;
+public class doWhile {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println("Value: "+ i);
+            i++;
+        }while(i<=5);
+    }
+}
